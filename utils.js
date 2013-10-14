2b805190458cf839733fed5800ad4e8f87ec79e3
Tanel Puhu <tanel@lusikas.com>
2013-10-14 23:13:39 +0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,32 +1,29 @@
-var get_multiplier = function() {
-        return parseFloat(localStorage.multiplier || '1');
-    },
-    load_options = function(){
-        console.log('setBadgeText')
-        $('#save').on('click', save_options);
-        $('#multiplier').val(get_multiplier());
-    },
-    save_options = function() {
-        var multiplier = $('#multiplier').val();
-        localStorage.multiplier = multiplier || '1';
-        reload_badge();
-    },
-    reload_badge = function () {
+var get_multiplier = function () {
+    return parseFloat(localStorage.multiplier || '1');
+},
+load_options = function () {
+    $('#save').on('click', save_options);
+    $('#multiplier').val(get_multiplier());
+},
+save_options = function () {
+    var multiplier = $('#multiplier').val();
+    localStorage.multiplier = multiplier || '1';
+    reload_badge();
+},
+reload_badge = function () {
+    chrome.browserAction.setBadgeText({
+        'text': '...'
+    });
+    $.getJSON("https://www.bitstamp.net/api/ticker/", function (data) {
+        if (!data && !data.last) {
+            return;
+        }
+        var value = parseFloat(data.last) * get_multiplier();
+        chrome.browserAction.setBadgeBackgroundColor({
+            color: [0, 0, 0, 255]
+        });
         chrome.browserAction.setBadgeText({
-            'text': '...'
+            'text': value.toFixed(2)
         });
-        $.getJSON("https://www.bitstamp.net/api/ticker/", function (data) {
-            if (!data && !data.last) {
-                return;
-            }
-            var value = parseFloat(data.last) * get_multiplier();
-            chrome.browserAction.setBadgeBackgroundColor({
-                color: [0, 0, 0, 255]
-            });
-            chrome.browserAction.setBadgeText({
-                'text': value.toFixed(2)
-            });
-        });
-    };
-
-
+    });
+};
